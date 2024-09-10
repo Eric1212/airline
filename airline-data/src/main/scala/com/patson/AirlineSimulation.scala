345f0c5f02eafa9989e78646f8486e5e0ee5abd1
Eric Boucher <bouchereric0000@hotmail.com>
2024-09-10 12:41:51 -0400

    Update AirlineSimulation.scala

--- airline-data/src/main/scala/com/patson/AirlineSimulation.scala
+++ airline-data/src/main/scala/com/patson/AirlineSimulation.scala
@@ -287,7 +287,7 @@ object AirlineSimulation {
         othersSummary.put(OtherIncomeItemType.DEPRECIATION, -1 * unassignedAirplanesDepreciation) //not a cash expense
 
         val negativeCashInterest = if (airlineValue.existingBalance < 0) {
-          (airlineValue.existingBalance * currentRate.annualRate / 52).toLong //give high interest
+          (airlineValue.existingBalance * getLoanOptions(principal, currentRate.annualRate, currentCycle) / 52).toLong //give high interest
         } else {
           0L
         }
