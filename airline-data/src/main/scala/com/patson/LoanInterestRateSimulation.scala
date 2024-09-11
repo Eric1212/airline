f29ff6f140c42f690e2bd124d68c080e54828582
Eric Boucher <bouchereric0000@hotmail.com>
2024-09-11 00:12:09 -0400

    Update LoanInterestRateSimulation.scala

--- airline-data/src/main/scala/com/patson/LoanInterestRateSimulation.scala
+++ airline-data/src/main/scala/com/patson/LoanInterestRateSimulation.scala
@@ -78,6 +78,7 @@ object LoanInterestRateSimulation {
   val BOUNDARY_ZONE_DELTA_ADJUSTMENT = 0.005 // 0.5% adjustment if it's considered in abnormal range (ie > HIGH or < LOW threshold)
   val HIGH_RATE_THRESHOLD = MAX_RATE - (MAX_RATE - MIN_RATE) * BOUNDARY_ZONE_FACTOR_HIGH
   val LOW_RATE_THRESHOLD = MIN_RATE + (MAX_RATE - MIN_RATE) * BOUNDARY_ZONE_FACTOR_LOW
+  var newRate : BigDecimal = 0.00
   
   def simulateNextRate(previousRate : BigDecimal, previousDelta : BigDecimal) : BigDecimal = {
      var newDelta : BigDecimal = (Random.nextInt((MAX_DELTA / RATE_STEP).toInt) + 1) * RATE_STEP
@@ -102,7 +103,7 @@ object LoanInterestRateSimulation {
       newDelta = MAX_DELTA * -1
     }
 
-     var newRate : BigDecimal = previousRate + newDelta
+     newRate = previousRate + newDelta
      if (newRate > MAX_RATE) {
        newRate = MAX_RATE
      } else if (newRate < MIN_RATE) {
@@ -110,9 +111,6 @@ object LoanInterestRateSimulation {
      }
      //BigDecimal(newPrice).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
      newRate
-    val FileWriter = new FileWriter(new File("EXPORT_newRate.txt"))
-FileWriter.write(newRate)
-FileWriter.close()
   }
-val OVERDRAFT_RATE : BigDecimal = scala.io.Source.fromFile("EXPORT_newRate.txt").mkString
+val OVERDRAFT_RATE : BigDecimal = newRate
 }
