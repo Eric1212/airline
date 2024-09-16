1fc049b03f309e4aa46613ce702f30b8bef1730b
Eric Boucher <bouchereric0000@hotmail.com>
2024-09-15 23:05:04 -0400

    Update start.sh
    Update for working start file.

--- airline-data/start.sh
+++ airline-data/start.sh
@@ -1,1 +1,1 @@
-java -Dactivator.home=/home/ubuntu/git/airline/airline-data -Dlog4j2.formatMsgNoLookups=true -Xms1024m -Xmx1024m -XX:MetaspaceSize=64m -XX:MaxMetaspaceSize=256m -jar /home/ubuntu/git/airline/airline-data/activator-launch-1.3.6.jar "runMain com.patson.MainSimulation" > airline-data.log &
+java -Dactivator.home=/var/www/flight.jcf.al/airline/airline-data -Dlog4j2.formatMsgNoLookups=true -Xms5000m -Xmx5000m -XX:MetaspaceSize=64m -XX:MaxMetaspaceSize=256m -jar /var/www/flight.jcf.al/airline/sbt-launch.jar "runMain com.patson.MainSimulation" > airline-data.log &
